54cc2c460f7c28f618768acf76354bbbcf91fb3f
Naresh240 <58024415+Naresh240@users.noreply.github.com>
2020-04-03 14:47:51 +0530

    Update dockerCleanup.groovy

--- vars/dockerCleanup.groovy
+++ vars/dockerCleanup.groovy
@@ -1,4 +1,3 @@
 def call(String project) {
-    sh "docker stack rm ${project}-beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
-    sh "docker system prune -f"
+    sh "docker rmi ${project}-beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
 }
