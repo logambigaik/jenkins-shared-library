b7147ed1c6644c7d0dbae4c1651187cbe71e19d3
Naresh240 <58024415+Naresh240@users.noreply.github.com>
2020-04-12 16:39:00 +0530

    Update dockerCleanup.groovy

--- vars/dockerCleanup.groovy
+++ vars/dockerCleanup.groovy
@@ -1,4 +1,3 @@
 def call(String project, String hubUser) {
-    sh "docker rmi ${hubUser}/${project}:${env.BUILD_NUMBER}"
-    sh "docker rmi ${hubUser}/${project}:latest"
+    sh "docker rmi ${hubUser}/${project}:${ImageName}"
 }
