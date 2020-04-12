3629a21a7fdbb61c8fdb25dcbb73dfd423b16666
Naresh240 <58024415+Naresh240@users.noreply.github.com>
2020-04-12 16:25:14 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String project, String hubUser) {
     sh "docker image build -t ${hubUser}/${project} ."
-    sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${env.BUILD_NUMBER}"
+    sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
     withCredentials([usernamePassword(
             credentialsId: "docker",
@@ -9,6 +9,6 @@ def call(String project, String hubUser) {
     )]) {
         sh "docker login -u '$USER' -p '$PASS'"
     }
-    sh "docker image push ${hubUser}/${project}:${env.BUILD_NUMBER}"
+    sh "docker image push ${hubUser}/${project}:${ImageTag}"
     sh "docker image push ${hubUser}/${project}:latest"
 }
